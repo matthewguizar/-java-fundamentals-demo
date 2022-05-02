f39ccf1f92e1fb088411931add33578f2ad216ef
Matthew Guizar <matthewrguizar@gmail.com>
2022-05-02 09:55:03 -0700

    added droid class with constructor, get/set methods, and a toString method to the droid class

---
+++ src/java_fundamentals_demo/Droid.java
@@ -0,0 +1,26 @@
+package java_fundamentals_demo;
+
+public class Droid {
+  private String serialNumber;
+  private String model;
+  private String purpose;
+
+  public Droid (String serialNumber, String model, String purpose) {
+    this.serialNumber = serialNumber;
+    this.model = model;
+    this.purpose = purpose;
+  }
+
+  public String getSerialNumber() { return this.serialNumber;}
+  public void setSerialNumber(String serialNumber) {this.serialNumber = serialNumber;}
+
+  public String getModel() {return this.model;}
+  public void setModel(String model) { this.model = model;}
+
+  public String getPurpose() {return this.purpose;}
+  public void setPurpose(String purpose) {this.purpose = purpose;}
+
+  public void speak(){System.out.println("WAAAAHHH");}
+  public String toString() {return "Droid{Serial Number: '" + serialNumber + "', Model Type: '" + model + " ', Purpose: '" + purpose + "}" ;}
+
+}
---
+++ src/java_fundamentals_demo/Main.java
@@ -0,0 +1,10 @@
+package java_fundamentals_demo;
+
+public class Main {
+    public Main(){
+
+    }
+    public static void main(String[] args){
+
+    }
+}
